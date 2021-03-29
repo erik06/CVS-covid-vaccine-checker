d13deabbf47bfc690d530e6d7d14026ca473c6b0
Erik Slagter <erikslagter@gmail.com>
2021-03-28 19:57:48 -0500

    Black Formatter ran, added Pipenv for virtual env and easy installation, format for Chicago, add 20 loops to the alarm/noise and auto launching browser to correct page via new url variable. Runs every 5 mins now for 24 hours

--- vaccine.py
+++ vaccine.py
@@ -1,4 +1,4 @@
-'''
+"""
 This is a python script that requires you have python installed, or in a cloud environment.
 
 This script scrapes the CVS website looking for vaccine appointments in the cities you list.
@@ -9,42 +9,58 @@ If you receive an error that says something is not install, type
 pip install beepy
 
 in your terminal.
-'''
 
+Source:
+https://python.plainenglish.io/how-i-built-a-cvs-vaccine-appointment-availability-checker-in-python-6beb379549e4
+"""
 
 
 import requests
 import time
 import beepy
+import subprocess
 
 
 def findAVaccine():
-    hours_to_run = 3 ###Update this to set the number of hours you want the script to run.
-    max_time = time.time() + hours_to_run*60*60
+    hours_to_run = 24
+    # Update this to set the number of hours you want the script to run.
+    max_time = time.time() + hours_to_run * 60 * 60
     while time.time() < max_time:
 
-        state = 'IL' ###Update with your state abbreviation. Be sure to use all CAPS, e.g. RI
+        state = "IL"  ###Update with your state abbreviation. Be sure to use all CAPS, e.g. RI
 
-        response = requests.get("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(state.lower()), headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
+        url = "https://www.cvs.com/immunizations/covid-19-vaccine"
+
+        response = requests.get(
+            "https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(
+                state.lower()
+            ),
+            headers={"Referer": url},
+        )
         payload = response.json()
 
         mappings = {}
         for item in payload["responsePayloadData"]["data"][state]:
-            mappings[item.get('city')] = item.get('status')
+            mappings[item.get("city")] = item.get("status")
 
         print(time.ctime())
-        cities = ['BELLEVILLE', 'CHICAGO', 'DEKALB', 'WAUKEGAN'] ###Update with your cities nearby
+        cities = ["CHICAGO"]  ###Update with your cities nearby
         for city in cities:
             print(city, mappings[city])
 
         for key in mappings.keys():
-            if (key in cities) and (mappings[key] != 'Fully Booked'):
-                beepy.beep(sound = 'coin')
+            if (key in cities) and (mappings[key] != "Fully Booked"):
+                subprocess.Popen(["open", url])
+                for _ in range(20):  # repeat sound 20 times
+                    beepy.beep(sound="coin")
                 break
             else:
                 pass
 
-        time.sleep(60) ##This runs every 60 seconds. Update here if you'd like it to go every 10min (600sec)
-        print('\n')
+        time.sleep(60)
+        # This runs every 60 seconds. Update here if you'd like it to go every 5min (300sec)
+        print("\n")
 
-findAVaccine() ###this final line runs the function. Your terminal will output the cities every 60seconds
+
+findAVaccine()  ###this final line runs the function. Your terminal will output the cities every 60seconds
+
